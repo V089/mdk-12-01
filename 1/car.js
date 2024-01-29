65cfcaf70fbc804ad9d38d2f477bf981c0270189
V089 <twinkt92@gmail.com>
2024-01-30 01:11:19 +0900

    first commit

---
+++ 1/car.js
@@ -0,0 +1,18 @@
+const carName = 'Lamborgini';
+let maxCarSpeed = '320 km/h';
+let carOwner = 'Owner';
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+maxCarSpeed = '325 km/h';
+carOwner = 'Вячеслав';
+console.log('У машины изменилась максимальная скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+maxCarSpeed = '310 km/h';
+carOwner = 'Станислав';
+console.log('У машины изменилась максимальная скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+maxCarSpeed = '340 km/h';
+carOwner = 'Дмитрий';
+console.log('У машины изменилась максимальная скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
---
+++ 1/task1.1.js
@@ -0,0 +1,16 @@
+//JavaScript код с созданием переменных
+
+/*
+Author
+V089
+*/
+const myName = 'Victor';
+const mySurname = '';
+let myFavoriteDrink = 'Tea';
+let myFavoriteAnimal = 'Cat';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
