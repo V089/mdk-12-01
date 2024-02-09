e8cbdeafaef4b2620536e6bcda3f00044fef5ef2
V089 <76247567+V089@users.noreply.github.com>
2024-02-09 00:37:09 +0000

    complete 4.1

---
+++ 4/4.1.js
@@ -0,0 +1,7 @@
+const myName = 'V089';
+const programmingLanguage = 'JavaScript';
+const courseCreatorName = 'Teacher';
+const reasonText = 'money';
+const numberOfMonth = 1;
+const result = 'Всем привет! Меня зовут, ${MyName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${programmingLanguage} ${numberOfMonth} месяц. Я уверен, что пройду данный курс до конца!';
+console.log(result);
