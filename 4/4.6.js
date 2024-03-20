04edf66b0c63704bb1e459531544ea6d74ec5868
V089 <twinkt92@gmail.com>
2024-03-20 12:37:26 +0900

    complete 4.6

---
+++ 4/4.6.js
@@ -0,0 +1,5 @@
+let userText = prompt('Введите текст').trim();
+let wordFromText = prompt('Введите слово из текста').trim();
+let indexOfWord = userText.indexOf(wordFromText);
+userText = userText.slice(0, indexOfWord);
+alert('Результат: ${userText}');
