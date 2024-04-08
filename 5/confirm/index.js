34d90f1ad80afd7f12d82b9f4ee9aa34a8910799
V089 <twinkt92@gmail.com>
2024-04-09 08:57:33 +0900

    complete 5.3

---
+++ 5/confirm/index.js
@@ -0,0 +1,17 @@
+if(confirm('JavaScript появился в 1995 году?')){
+    alert('Верно');
+}else{
+    alert('Неверно');
+}
+
+if(confirm('Спецификация JavaScript называется ECMAScript?')){
+    alert('Верно');
+}else{
+    alert('Неверно');
+}
+
+if(confirm('JavaScript был создан за 1 месяц?')){
+    alert('Неверно');
+}else{
+    alert('Верно');
+}
